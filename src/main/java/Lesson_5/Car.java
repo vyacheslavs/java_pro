36fc6404391759cec23969a36a38dd0ca76cbfc6
Shegai Vyacheslav <v.shegai@netris.ru>
2019-06-21 10:46:06 +0300

    добавлено решение к уроку 5

--- src/main/java/Lesson_5/Car.java
+++ src/main/java/Lesson_5/Car.java
@@ -1,15 +1,18 @@
 package Lesson_5;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static int WINNER_CAR = -1;
     static {
         CARS_COUNT = 0;
     }
     private Race race;
     private int speed;
+    private int carNo;
+    private static ReentrantLock lock = new ReentrantLock();
     private String name;
     private CountDownLatch barrier;
     public String getName() {
@@ -24,6 +27,7 @@ public class Car implements Runnable {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
+        this.carNo = CARS_COUNT;
         this.name = "Участник #" + CARS_COUNT;
     }
     @Override
@@ -41,5 +45,12 @@ public class Car implements Runnable {
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+
+        lock.lock();
+        if (WINNER_CAR == -1) {
+            WINNER_CAR = carNo;
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Победитель: участник #"+carNo);
+        }
+        lock.unlock();
     }
 }
--- src/main/java/Lesson_5/MainClass.java
+++ src/main/java/Lesson_5/MainClass.java
@@ -1,14 +1,14 @@
 package Lesson_5;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
-    public static final int CARS_COUNT = 4;
+    public static final int CARS_COUNT = 10;
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+        Thread [] th = new Thread[CARS_COUNT];
 
         CountDownLatch cd = new CountDownLatch(CARS_COUNT);
 
@@ -16,12 +16,18 @@ public class MainClass {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cd);
         }
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+            th[i] = new Thread(cars[i]);
+            th[i].start();
         }
 
         try {
             cd.await();
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+            for (int i = 0; i < cars.length; i++) {
+                th[i].join();
+            }
+
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
         } catch (InterruptedException e) {
             e.printStackTrace();
